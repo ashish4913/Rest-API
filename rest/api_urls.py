3fb2307a733d5be9b5ca7986e149e03e4b0e3552
ashish1304 <ashish.jain1304@gmail.com>
2020-06-28 16:08:49 +0530

    generic api

--- rest/api_urls.py
+++ rest/api_urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     #path('user/<int:id>',views.test_details) 
     #for class base api url
     path('user/',views.testclass.as_view()),
-    path('user/<int:id>',views.testclassdetails.as_view())
+    path('user/<int:id>',views.testclassdetails.as_view()),
+
+
+    #generic api urls
+    path('generic/',views.testgeneric.as_view()),
+    path('generic/<int:id>/',views.testgeneric.as_view())
+
 ]
--- rest/views.py
+++ rest/views.py
@@ -6,6 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import mixins
 # Create your views here. """
 
 # this is function based views
@@ -101,3 +103,33 @@ class testclassdetails(APIView):
         u=u=self.getobject(id)
         u.delete()
         return HttpResponse(status=204)
+
+
+
+
+
+
+
+#generic api
+
+
+class testgeneric (generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,mixins.RetrieveModelMixin):
+    serializer_class=userserializer
+    queryset=user.objects.all()
+    lookup_field="id"
+
+    def get(self,request,id=None):
+        if id:
+            return self.retrieve(request,id) 
+        else:
+            return self.list(request)  
+    
+    def post(self,request):
+        return self.create(request)
+    
+    def put(self,request,id=None):
+        return self.update(request,id)
+
+
+    def delete(self,request,id=None):
+        return self.destroy(request,id)
